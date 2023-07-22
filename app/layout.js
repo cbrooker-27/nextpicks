ccc23b8b011cfbb1546a8b1b7436cfb3e386d627
Chris Brooker <cbrooker27@gmail.com>
2023-07-21 22:52:16 -0400

    following meta conventions

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,12 @@ import MainNavigation from "@/components/layout/main-navigation";
 export const metadata = {
   title: "ChrisBrooker.com",
   description: "New and improved site written in React",
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
       <body >
         <MainNavigation/>
         <main>{children}</main>
