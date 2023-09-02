c5ba92bbc4b17202b25e5d42c32ac5eddf523f3a
Chris Brooker <cbrooker27@gmail.com>
2023-09-02 19:24:31 -0400

    Adding links to the picks page

--- app/picks/page.js
+++ app/picks/page.js
@@ -1,3 +1,11 @@
-export default function PicksMain(){
-    return <h1>Future home of the new and improved picks</h1>
+export default function PicksMain() {
+  return (
+    <div>
+      <h1>Future home of the new and improved picks</h1>
+      <br />
+      <a href="http://69.206.177.181/servlets/Picks">Old School</a>
+      <br />
+      <a href="http://69.206.177.181/picks/mobile">Mobile Version</a>
+    </div>
+  );
 }
