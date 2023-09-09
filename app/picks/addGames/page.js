45acb2e6e2a148bc318f9ae66f5e0d361b21ea36
Chris Brooker <cbrooker27@gmail.com>
2023-09-09 15:08:45 -0400

    adding debug for MSF failure

--- app/picks/addGames/page.js
+++ app/picks/addGames/page.js
@@ -9,8 +9,11 @@ export default async function AddGames(){
     //const msfUrl = "https://api.mysportsfeeds.com/v2.1/pull/nfl/"+getSeason()+"-regular/week/" + getWeek() + "/games.json"
     const msfUrl = "https://api.mysportsfeeds.com/v2.1/pull/nfl/2023-2024-regular/week/1/games.json"
     const headers = new Headers();
+    console.log(process.env.MYSPORTSFEEDS_CREDS);
     headers.append("Authorization",'Basic '+ Buffer.from(""+process.env.MYSPORTSFEEDS_CREDS).toString('base64'));
+    console.log(headers)
     const response = await fetch(msfUrl,{method:'GET', headers:headers})
+    console.log(response)
     const resJson = await response.json()
     const games = resJson.games;
     const teams = resJson.references.teamReferences;
