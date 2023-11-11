d2b0885d36788bf4d9eccc4b7efdc3ac7d67a696
Chris Brooker <cbrooker27@gmail.com>
2023-11-11 17:29:04 -0500

    new viewport export paradigm

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,13 @@ import Footer from "@/components/layout/footer";
 export const metadata = {
   title: "ChrisBrooker.com",
   description: "New and improved site written in React",
-  viewport: "width=device-width, initial-scale=1",
 };
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+}
 
 export default function RootLayout({ session, children }) {
   return (
