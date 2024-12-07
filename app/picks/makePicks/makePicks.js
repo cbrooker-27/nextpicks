298794ccf50bd70433cdb725e9d13c1e5ae9d6a5
Chris Brooker <cbrooker27@gmail.com>
2024-12-06 21:21:47 -0500

    update packages

--- app/picks/makePicks/makePicks.js
+++ app/picks/makePicks/makePicks.js
@@ -4,7 +4,7 @@ import MakePicksForm from "./makePicksForm";
 
 export default async function MakePicks(){
     /* On load, go get games from our database */
-    const thisWeeksGames = await getPickableGames(1);
+    const thisWeeksGames = await getPickableGames(14);
 
     return (<MakePicksForm games={thisWeeksGames}/>
       );
