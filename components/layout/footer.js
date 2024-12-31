3f6ac68a96113c54e9a95b82b53f7d693dde9f00
Chris Brooker <cbrooker27@gmail.com>
2024-12-31 06:20:47 -0500

    updates to footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,17 +1,5 @@
-'use client'
-import Link from "next/link";
-import Image from "next/image";
-import { useSession, signOut } from "next-auth/react";
-
 import classes from "./footer.module.css";
 
 export default function Footer() {
-  
-  return (
-    <footer className={classes.footer}>
-      I'm a little teapot
-    </footer>
-  );
+  return <footer className={classes.footer}>Standard Footer</footer>;
 }
-
-
