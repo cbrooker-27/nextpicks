e5253ed9fcfd4346bee9889b05ac218904ac3b2e
Chris Brooker <cbrooker27@gmail.com>
2024-12-31 08:40:30 -0500

    adding responsive drawer

--- app/layout.js
+++ app/layout.js
@@ -9,22 +9,23 @@ export const metadata = {
 };
 
 export const viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
-}
+};
 
 export default function RootLayout({ session, children }) {
-  //<MySessionProvider session={session}></MySessionProvider>
   return (
     <html lang="en">
       <body>
-        
+        <MySessionProvider session={session}>
           <MainNavigation />
-          <main style={{ marginTop: "100px", marginBottom: "33px"}}>{children}</main>
+          <main style={{ marginTop: "100px", marginBottom: "33px" }}>
+            {children}
+          </main>
           <div id="notifications"></div>
           <Footer />
-    
+        </MySessionProvider>
       </body>
     </html>
   );
--- components/games/pickableGameTile.js
+++ components/games/pickableGameTile.js
@@ -1,5 +1,10 @@
 import cssStyles from "./pickableGameTile.module.css";
 import TeamTile from "../teams/teamTile";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
 export default function PickableGameTile({ game }) {
   // const game = props.game;
@@ -15,6 +20,21 @@ export default function PickableGameTile({ game }) {
           <TeamTile className={cssStyles.gameteam} team={game.away} />
         </div>
       </div>
+      <FormControl>
+        <FormLabel id={`pickradiogroup-${game._id}-label`}>Pick</FormLabel>
+        <RadioGroup
+          aria-labelledby={`pickradiogroup-${game._id}-label`}
+          name={`pickradiogroup-${game._id}`}
+        >
+          <FormControlLabel
+            value="ff"
+            control={<Radio />}
+            label={`${game.home.name} will win by more than ${game.spread}`}
+          />
+          <FormControlLabel value="uf" control={<Radio />} label="uf" />
+          <FormControlLabel value="uu" control={<Radio />} label="uu" />
+        </RadioGroup>
+      </FormControl>
       <div className={cssStyles.gamefooter}>
         {game.location} - <p className={cssStyles.gameid}>{game._id}</p>
       </div>
