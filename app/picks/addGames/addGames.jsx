cd6d1c553a269896fc097d2a7cbacd972268fccd
Chris Brooker <cbrooker27@gmail.com>
2025-10-14 19:24:09 -0400

    make add games client side again until i understand serverside rendering

--- app/picks/addGames/addGames.jsx
+++ app/picks/addGames/addGames.jsx
@@ -1,29 +1,34 @@
+"use client";
 import { getTeamStatisticsFromMsf, getThisWeeksGamesFromMsf } from "@/app/lib/msf.js";
 import AddGamesForm from "./addGamesForm";
 import { Skeleton } from "@mui/material";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getCurrentWeek, getAllGames } from "@/app/utils/db";
 import { SeasonStatisticsProvider } from "@/app/context/SeasonStatistics";
 
-export default async function AddGames() {
-  // const [thisWeeksGames, setThisWeeksGames] = useState([]);
-  // const [teamDetails, setTeamDetails] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
+export default function AddGames() {
+  const [thisWeeksGames, setThisWeeksGames] = useState([]);
+  const [teamDetails, setTeamDetails] = useState([]);
+  const [seasonData, setSeasonData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // useEffect(() => {
-  // async function fetchData() {
-  const week = await getCurrentWeek();
-  const thisWeeksGames = await getThisWeeksGamesFromMsf();
-  const teamDetails = await getTeamStatisticsFromMsf(week);
-  const seasonData = await getAllGames(week.season);
-  // setThisWeeksGames(fetchedGames);
-  // setTeamDetails(teamDetails);
-  // setIsLoading(false);
-  // }
-  // fetchData();
-  // }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const week = await getCurrentWeek();
+      const fetchedGames = await getThisWeeksGamesFromMsf();
+      const teamDetails = await getTeamStatisticsFromMsf(week);
+      const seasonData = await getAllGames(week.season);
+      setThisWeeksGames(fetchedGames);
+      setTeamDetails(teamDetails);
+      setSeasonData(seasonData);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
 
-  return (
+  return isLoading ? (
+    <Skeleton />
+  ) : (
     <SeasonStatisticsProvider value={{ seasonData: seasonData }}>
       <AddGamesForm games={thisWeeksGames} teamDetails={teamDetails} />
     </SeasonStatisticsProvider>
