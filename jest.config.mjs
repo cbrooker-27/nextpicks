2b0cd5c7eb85cabf44002eed9231cda0e789b4e2
Chris Brooker <cbrooker27@gmail.com>
2026-05-25 07:25:11 -0400

    update test configuration

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,31 +1,32 @@
-import nextJest from 'next/jest.js'
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
   // Optional: configure coverage or ignore patterns
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
-    '^@/(.*)$': '<rootDir>/$1',
-  }
-}
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async () => {
   const nextConfig = await createJestConfig(config)();
-  
+
   nextConfig.transformIgnorePatterns = [
-    '/node_modules/(?!(bson|mongodb|mongodb-connection-string-url|whatwg-url|tr46|webidl-conversions)/)'
+    "/node_modules/(?!(bson|mongodb|mongodb-connection-string-url|whatwg-url|tr46|webidl-conversions)/)",
   ];
 
   return nextConfig;
-}
+};
